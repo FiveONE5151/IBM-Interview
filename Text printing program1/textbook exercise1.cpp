e5958feb682447addab41f02d48ad4de3ce8868c
FiveONE5151 <114002722+FiveONE5151@users.noreply.github.com>
2022-09-20 23:24:17 +0800

    v1.1
    test

---
+++ Text printing program1/textbook exercise1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>//allows program to output data to the screen
+
+
+//function main begins program execution
+int main()
+{
+    std::cout<<"welcome to c++!\n";//display message
+    system("pause"); //to pause the display message   
+  //return can be left out
+}//end function main
