70aabcee271624677936b013e2115383886e6c63
FiveONE5151 <2310199261@qq.com>
2022-09-22 22:54:21 +0800

    fix1

---
+++ Text printing program1/no2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of char is " << sizeof(char) <<endl;
+    cout << "Size of short int is " << sizeof(char) <<endl;
+    cout << "Size of int is " << sizeof(int) <<endl;
+    cout << "Size of double is " << sizeof(double) <<endl;
+    cout << "Size of long double is " << sizeof(long double) <<endl;
+    
+}
